11bf331ce7c32f29baac1f2af6bdbf2f63d402b4
Emmanuel-Ezekiel <emamex44@gmail.com>
2021-09-07 22:47:16 +0100

    add functions

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import Review from './Review'
 
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2> Reviews </h2>
+          <Review />
+        </div>
+      </section>
+    </main>
+  
+  );
 }
 
 export default App;
